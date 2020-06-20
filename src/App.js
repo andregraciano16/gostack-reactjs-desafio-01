fc274b896ca5c534da850281a039996b113e2548
andregraciano16 <andregraciano16@gmail.com>
2020-06-20 02:22:29 -0300

    terminando desafio reactjs

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: `${Date.now()}`,
+      url: "http://github.com13",
+      techs: ["Angular.js", "JAVA"]
+    });
+    setRepositories([...repositories, response.data])
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(repository => repository.id !== id))
+    console.log(repositories)
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (<li key={repository.id}>
+          {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover
+          </button>
+        </li>))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
